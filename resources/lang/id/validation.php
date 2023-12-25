e478fb9c020d983b76f32418d8663834303749a3
Ahmad Taufiq <ataufiq665@gmail.com>
2023-12-25 10:15:31 +0700

    add error alert and change localization to Indonesia

---
+++ resources/lang/id/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |---------------------------------------------------------------------------------------
+    | Baris Bahasa untuk Validasi
+    |---------------------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut ini berisi standar pesan kesalahan yang digunakan oleh
+    | kelas validasi. Beberapa aturan mempunyai banyak versi seperti aturan 'size'.
+    | Jangan ragu untuk mengoptimalkan setiap pesan yang ada di sini.
+    |
+    */
+    'required'             => ':attribute wajib diisi.',
+    'integer'  => ':attribute harus berupa bilangan bulat.',
+];
+// Source: https://github.com/Laravel-Lang/lang
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,6 +20,22 @@
                 </div>
             </div>
             @endif
+            @if ($errors->any())
+              <div class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-200 dark:bg-gray-800 dark:text-red-400" role="alert">
+                <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                </svg>
+                <span class="sr-only">Danger</span>
+                <div>
+                  <span class="font-medium">Pastikan persyaratan berikut terpenuhi:</span>
+                    <ul class="mt-1.5 list-disc list-inside">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                  </ul>
+                </div>
+              </div>
+            @endif
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg mb-3">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
